9caa98801b16502cba6743334e4dd96ac50b833c
Kevin-Clement <kevin-clement@live.fr>
2021-05-13 22:05:04 +0200

    Premier ajout btn supp

--- front-end/js/cart.js
+++ front-end/js/cart.js
@@ -3,10 +3,10 @@ let articleInLocalStorage = JSON.parse(localStorage.getItem("article"));
 // console.log(articleInLocalStorage);
 
 const productCart = document.getElementById("products-tablebody");
-console.log(productCart);
+// console.log(productCart);
 
 //Si le panier est vide 
-if (articleInLocalStorage === null) {
+if (articleInLocalStorage === null || articleInLocalStorage == 0) {
     const emptyCart = `
     <tr id="title-cart-empty" class="col-12 mx-auto">
         <td></td>
@@ -16,17 +16,50 @@ if (articleInLocalStorage === null) {
     productCart.innerHTML = emptyCart;
 } else {
     let structureCart = [];
-    for (i = 0; i < articleInLocalStorage.length; i++){
+    for (i = 0; i < articleInLocalStorage.length; i++) {
         structureCart += `
         <tr class="col-12">
             <td class="text-center"><strong>${articleInLocalStorage[i].name}</strong></td>
             <td class="text-center">1</td>
             <td class="text-center">${(articleInLocalStorage[i].price)}.00 €</td>
+            <td class="text-center removeArticle"><button class="btn-trash"><i class="fas fa-trash-alt"></button></td>
         </tr>`;
     }
-        if(i == articleInLocalStorage.length){
+    if (i == articleInLocalStorage.length) {
         productCart.innerHTML = structureCart;
     }
+
 }
+
+//*********************Bouton supprimer************************
+//Selection du bouton
+
+let removeArticle = document.querySelectorAll(".removeArticle");
+console.log(removeArticle);
+
+for(let k = 0; k < removeArticle.length; k++){
+    removeArticle[k].addEventListener("click",(e) =>{
+        e.preventDefault();
+
+        let removeIdSelect = articleInLocalStorage[k].theId;
+        // attention probleme suppression de tous les memes id 
+        articleInLocalStorage = articleInLocalStorage.filter( elt => elt.theId !== removeIdSelect);
+        console.log(articleInLocalStorage)
+
+        // stringify() convertit une valeur JavaScript en chaîne JSON et envoie dans la key "article" du localStorage
+        localStorage.setItem("article", JSON.stringify(articleInLocalStorage));
+        window.location.reload();
+    })
+}
+
+// removeArticle.addEventListener("click", () => {this.annulerArticle(i);})
+// annulerArticle = (i) => {
+//     articleInLocalStorage.splice(i, 1);
+//     localStorage.clear();
+//     // Mise à jour du nouveau panier avec suppression de l'article
+//     localStorage.setItem("article", JSON.stringify(articleInLocalStorage));
+//     //Mise à jour de la page pour affichage de la suppression au client
+//     window.location.reload();
+// };
 
 
