0d989d683b0da35e405d6579ce6b027b32c7f6f8
Kevin-Clement <kevin-clement@live.fr>
2021-11-05 23:30:53 +0100

    ajustement appel API

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,45 +1,33 @@
-//************************fonction main qui s'execute des le chargement de la page****************
 
-(async function () {
-    //On attend les données avec await de la promesse fetch
-    const articles = await getArticles();
-    for (article of articles) {
-        displayArticles(article);
-    }
-})();
-
-//**********************************Récupération des articles**************************************
-
-function getArticles() {
-    //On return tout le fetch qui est une promesse qui n'est pas encore résolue
-    return fetch("http://localhost:3000/api/cameras")
-        //Fonction qu'il execute a la récupération des données
-        //transformation en json du paramètre
-        .then(function (httpBodyResponse) {
-            return httpBodyResponse.json()
-        })
-        //Récupération de tous les articles
-        .then(function (articles) {
-            return articles
-        })
-        //Au cas ou il ne marche pas (si l'API est down par exemple)
-        .catch(function (error) {
-            alert(error)
-        })
-};
-
-function displayArticles(article) {
-    document.getElementById("articles").innerHTML += `
-        <div class="card col-lg-5 col-md-12 col-sm-12 col-12 mt-4 mb-4 pt-3 pb-3 shadow">
-            <a href="/front-end/product.html?id=${article._id}" class="card text-decoration-none text-reset">
-                <div class="card-img">
-                    <img class="card-img-top" src="${article.imageUrl}" alt="">
-                </div>
-                <div class="card-body">
-                    <p class="h5 card-title"><strong>${article.name}</strong> ${(article.price)/100}.00 €</p>
-                    <p class="card-text">${article.description}</p>
-                </div>
-                <button class="col-lg-7 col-md-4 col-7 mt-2 mb-4 mx-auto btn-infocart">Plus d'informations</button>
-            </a>
-        </div>`;
+async function getArticles() {
+    let articles = await fetch("http://localhost:3000/api/cameras")
+    return await articles.json();
 }
+
+async function displayArticles() {
+    await getArticles ()
+    .then(function (resultAPI){
+        const articles = resultAPI;
+        console.table(articles);
+        for (let article in articles) {
+            document.getElementById("articles").innerHTML +=
+                    `<div class="card col-lg-5 col-md-12 col-sm-12 col-12 mt-4 mb-4 pt-3 pb-3 shadow">
+                    <a href="/front-end/product.html?id=${resultAPI[article]._id}" class="card text-decoration-none text-reset">
+                        <div class="card-img">
+                            <img class="card-img-top" src="${resultAPI[article].imageUrl}" alt="">
+                        </div>
+                        <div class="card-body">
+                            <p class="h5 card-title"><strong>${resultAPI[article].name}</strong> ${(resultAPI[article].price)/100}.00 €</p>
+                            <p class="card-text">${resultAPI[article].description}</p>
+                        </div>
+                        <button class=" mt-2 mb-4 mx-auto btn-infocart">Plus d'informations</button>
+                    </a>
+                </div>`
+        }
+    })
+    .catch (function(error){
+        return error;
+    });
+}
+
+displayArticles();
